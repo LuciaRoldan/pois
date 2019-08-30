21055ccc1be89528dc671eaa8747018e3dbf8c5a
Alumno <alumno@labsistemas.dom>
2019-08-30 18:17:15 -0300

    fsd

--- src/main/java/ar/com/curso/poi/modelo/POI.java
+++ src/main/java/ar/com/curso/poi/modelo/POI.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.PROPERTY)
